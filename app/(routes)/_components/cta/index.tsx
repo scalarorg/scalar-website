f4ed338df90d5d452489b6397d13b09e2d05e1ec
Truong Quoc Hoi <hoitrnd107@gmail.com>
2024-01-26 17:18:43 +0700

    feat: Validate form submit subcribe in CTA component

--- app/(routes)/_components/cta/index.tsx
+++ app/(routes)/_components/cta/index.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import { cn } from "../../../../lib/utils";
 import { cva, VariantProps } from "class-variance-authority";
 import { HTMLAttributes } from "react";
@@ -7,6 +9,7 @@ import SectionTitle from "@/components/ui/section-title";
 import { CarouselLogo } from "./components/carousel";
 import { Hackathon } from "./components/hackathon";
 import { HackathonMobile } from "@/app/(routes)/_components/cta/components/hackathon-mobile";
+import { Resolver, useForm } from "react-hook-form";
 
 const ctaVariants = cva(
   "relative z-0 py-[117px]  gap-[60px] w-full overflow-hidden bg-star bg-center bg-no-repeat bg-cover flex flex-col items-center justify-around",
@@ -17,7 +20,29 @@ type CtaVariantProps = VariantProps<typeof ctaVariants>;
 type CtaProps = CtaVariantProps &
   Omit<HTMLAttributes<HTMLDivElement>, keyof CtaVariantProps>;
 
+type FormValues = {
+  email: string
+}
+
+const resolver: Resolver<FormValues> = async (values) => {
+  return {
+    values: values.email ? values : {},
+    errors: !values.email
+      ? {
+        email: {
+          type: "required",
+          message: "This is required.",
+        },
+      }
+      : {},
+  }
+}
+
 export function Cta({ className }: CtaProps) {
+  const { register, handleSubmit, formState: { errors }, } = useForm<FormValues>({ resolver })
+
+  const onSubmit = handleSubmit((data) => console.log(data))
+
   return (
     <div className={ctaVariants({ className })}>
       {/* Hackathons & Grants Won Section */}
@@ -54,7 +79,7 @@ export function Cta({ className }: CtaProps) {
             )}
           >
             Don't Miss Out,{" "}
-            <span className={cn("text-cyan-500")}>Subscribe</span> Today!
+            <span className={cn("text-primary-cyan-500")}>Subscribe</span> Today!
           </label>
           <label
             className={cn("text-lg leading-[27px] text-neutral-4 text-left")}
@@ -64,25 +89,39 @@ export function Cta({ className }: CtaProps) {
             project. Join us at the forefront of new discoveries!
           </label>
         </div>
-        <form className={cn("relative")}>
-          <Input
-            className={cn(
-              "bg-transparent h-[48px] md:h-[93px] outline-none text-neutral-5 text-[18px] leading-[27px] flex justify-between items-center rounded-xl border border-cyan-400 px-[16px] lg:py-[16px]",
-            )}
-            placeholder="Your email"
-            type="email"
-          />
 
-          <Button
-            itemType="submit"
-            className={cn(
-              "absolute top-1/2 -translate-y-1/2 right-[20px] py-[6px] px-[20px] lg:py-[14px] lg:px-[32px] bg-cyan-400 text-white rounded-lg border-none",
-            )}
-          >
-            <span className="lg:text-[22px] lg:leading-[33px] lg:text-neutral-10">
-              Subscribe
-            </span>
-          </Button>
+        {/* Form */}
+        <form
+          className={cn("flex flex-col gap-[10px]")}
+          onSubmit={onSubmit}
+        >
+          <div className={cn("relative")} >
+            <Input
+              className={cn(
+                `bg-transparent h-[48px] md:h-[93px] text-neutral-5 text-[18px] leading-[27px] flex justify-between items-center rounded-xl border border-primary-cyan-500 px-[16px] lg:py-[16px] focus:border-2 focus:border-primary-cyan-500 hover:border-primary-cyan-800 focus:shadow-primary-cyan-500
+                ${errors.email && 'border-accent-warning-500'}
+                `
+              )}
+              placeholder="Your email"
+              type="email"
+              {...register('email')}
+            />
+
+            <Button
+              itemType="submit"
+              className={cn(
+                `absolute top-1/2 -translate-y-1/2 right-[20px] py-[6px] px-[20px] lg:py-[14px] lg:px-[32px] bg-primary-cyan-500 text-white rounded-lg border-none
+                ${errors.email && 'bg-accent-warning-50'}                
+                `,
+              )}
+            >
+              <span className="lg:text-[22px] lg:leading-[33px] lg:text-neutral-10">
+                Subscribe
+              </span>
+            </Button>
+          </div>
+
+          {errors?.email && <p className={cn("font-normal text-[18px] leading-[27px] text-accent-warning-500")}>{errors.email.message}</p>}
         </form>
       </div>
     </div>
