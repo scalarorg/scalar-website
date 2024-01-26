a1cf28a8193cad34bf32ed97d50ddb27508ca15f
Truong Quoc Hoi <hoitrnd107@gmail.com>
2024-01-26 11:57:19 +0700

    chore: fix resonsive plant for small device

--- app/(v2)/new-version/_components/dag/components/scalar-advantage.tsx
+++ app/(v2)/new-version/_components/dag/components/scalar-advantage.tsx
@@ -15,7 +15,7 @@ import { ScalarAdvantageContent } from '@/lib/constants/scalar-advantage-content
 import AdvantageItem from './advantage-item';
 
 const ScalarAdvantageVariants = cva(
-  "relative z-10 flex justify-center mb-[60px] lg:h-screen lg:mb-[220px] border-0 xl:border-2 xl:border-neutral-11 xl:border-t-0 xl:border-l-0 xl:border-r-0 xl:rounded-[120%]",
+  "relative z-10 flex justify-center mb-[60px] lg:h-screen lg:mb-[220px] border-0 lg:border-2 lg:border-neutral-11 lg:border-t-0 lg:border-l-0 lg:border-r-0 lg:rounded-[120%]",
 );
 
 type ScalarAdvantageVariantProps = VariantProps<typeof ScalarAdvantageVariants>;
@@ -38,11 +38,17 @@ const ScalarAdvantage = ({ className }: ScalarAdvantageProps) => {
       {/* Background */}
       {/* Mobile */}
       <Image
+        src={GROW_EFFECT}
+        alt=''
+        sizes='100vw'
+        className={(cn('xl:hidden absolute select-none -z-10 -top-[22%] -left-[30%]'))}
+      />
+      <Image
         src={ADVANTAGE_EARTH_SMALL}
         alt="Earth"
         width={98}
         height={121}
-        className={cn("lg:hidden absolute -right-[10px] top-0 z-0")}
+        className={cn("lg:hidden absolute -right-[10px] top-1/4 z-0")}
       />
 
       <Image
@@ -72,7 +78,7 @@ const ScalarAdvantage = ({ className }: ScalarAdvantageProps) => {
         src={ADVANTAGE_MARS_LARGE}
         alt=''
         sizes='100vw'
-        className={(cn('hidden xl:block w-[234px] h-[247px] 2xl:w-[354px] 2xl:h-[367px] absolute select-none -z-10 left-0 bottom-0'))}
+        className={(cn('hidden lg:block w-[234px] h-[247px] 2xl:w-[354px] 2xl:h-[367px] absolute select-none -z-10 left-0 bottom-0'))}
       />
 
       <Image
@@ -81,7 +87,7 @@ const ScalarAdvantage = ({ className }: ScalarAdvantageProps) => {
         sizes='100vw'
         width={477}
         height={456}
-        className={(cn('hidden xl:block w-[341px] h-[327px] 2xl:w-[477px] 2xl:h-[456px] absolute select-none -z-10 left-[283px] 2xl:left-[472px] -bottom-[18%] 2xl:-bottom-[26%]'))}
+        className={(cn('hidden lg:block w-[341px] h-[327px] 2xl:w-[477px] 2xl:h-[456px] absolute select-none -z-10 left-[283px] 2xl:left-[472px] -bottom-[18%] 2xl:-bottom-[26%]'))}
       />
 
       <Image
@@ -89,7 +95,7 @@ const ScalarAdvantage = ({ className }: ScalarAdvantageProps) => {
         alt=''
         width={392}
         height={404}
-        className={(cn('hidden xl:block w-[322px] h-[347px] 2xl:w-[392px] 2xl:h-[404px] absolute select-none -z-10 right-0 top-[30%]'))}
+        className={(cn('hidden lg:block w-[322px] h-[347px] 2xl:w-[392px] 2xl:h-[404px] absolute select-none -z-10 right-0 top-[30%]'))}
       />
 
 
