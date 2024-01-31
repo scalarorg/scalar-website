f1db1e12b5cb791a4d4b515bfc2d7c74b7b3463e
Truong Quoc Hoi <hoitrnd107@gmail.com>
2024-01-31 10:46:58 +0700

    chore: change font-size of section title component

--- components/ui/section-title.tsx
+++ components/ui/section-title.tsx
@@ -3,7 +3,7 @@ import React, { HTMLAttributes } from "react";
 import { FadeIn } from "@/components/motion/fade-in";
 
 const sectionTitleVariants = cva(
-  "text-white text-[36px] leading-[52.5px] lg:text-[48px] lg:leading-[66px] xl:text-[67px] xl:leading-[83.75px] font-bold",
+  "text-white text-[31px] leading-[46.5px] lg:text-[48px] lg:leading-[66px] xl:text-[67px] xl:leading-[83.75px] font-bold",
 );
 
 type sectionTitleVariantProps = VariantProps<typeof sectionTitleVariants>;
