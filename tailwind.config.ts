b11c53b83b6a1389b437419fa12d80337c0e2a73
Duc Huong <duchuong007@gmail.com>
2024-02-02 14:48:05 +0700

    chore: update package

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,6 @@
 import type { Config } from "tailwindcss";
 
-import defaultTheme from "tailwindcss/defaulttheme";
+import defaultTheme from "tailwindcss/defaultTheme";
 
 const config = {
   // darkMode: ["class"],
